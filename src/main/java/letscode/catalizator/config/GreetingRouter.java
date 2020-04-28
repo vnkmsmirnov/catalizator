c7f27407bb416d1e5b9bb987998071c5dc35eaff
Mikhail Smirnov <vnk.m.smirnov@gmail.com>
2020-04-28 13:06:13 +0300

    add db

--- src/main/java/letscode/catalizator/config/GreetingRouter.java
+++ src/main/java/letscode/catalizator/config/GreetingRouter.java
@@ -4,12 +4,7 @@ import letscode.catalizator.handlers.GreetingHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserter;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.*;
-
-import java.util.Map;
-import java.util.Optional;
 
 @Configuration
 public class GreetingRouter {
